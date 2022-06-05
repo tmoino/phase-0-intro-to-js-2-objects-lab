0088b97f54be3ab61f805b362f99d35a9faaa37a
tmoino <tony.moino@student.moringaschool.com>
2022-06-05 22:54:24 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee={
+    name: "Moino", streetAdress: "98765-98",
+};
+function  updateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign ({}, employee, { [key]: value});
+   
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value;
+    return employee;
+
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newObj= Object.assign({},employee);
+    delete newObj[key];
+    return newObj;
+
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
